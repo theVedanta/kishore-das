fed7f4045f1e2110ea5b23ff536be27565619ddd
Vedanta Somnathe <vedanta1412@gmail.com>
2022-03-27 01:22:38 +0530

    fix

--- src/BASE_API_URL.js
+++ src/BASE_API_URL.js
@@ -1,3 +1,3 @@
-const BASE_API_URL = "https://kishore-api.herokuapp.com/";
-// const BASE_API_URL = "http://localhost:4000/";
+const BASE_API_URL = "https://kishore-api.herokuapp.com";
+// const BASE_API_URL = "http://localhost:4000";
 export default BASE_API_URL;
