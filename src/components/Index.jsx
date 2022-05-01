472e45fd37d72ff3fe7d67dadc968f08077e0bb1
Vedanta Somnathe <vedanta1412@gmail.com>
2022-05-01 09:02:33 +0530

    test netli

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -16,13 +16,13 @@ const notyf = new Notyf();
 
 const Index = () => {
     const [index, setIndex] = useState({});
-    const [loaded, setLoaded] = useState(false);
+    // const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         const initialFetch = async () => {
             try {
+                // setLoaded(true);
                 const dataJson = await fetch(`${BASE_API_URL}/api/cms`);
-                setLoaded(true);
                 const data = await dataJson.json();
                 const index = data.index;
 
@@ -34,7 +34,7 @@ const Index = () => {
                     once: true,
                 });
             } catch (err) {
-                setLoaded(false);
+                // setLoaded(false);
                 notyf.error("Something went wrong");
             }
         };
@@ -45,7 +45,7 @@ const Index = () => {
     return (
         <>
             <Nav />
-            <Load loaded={loaded}>
+            <Load loaded={index ? index.header : false}>
                 <main className="relative">
                     <div className="main-container px-72 pt-16 blap:px-52 lap:px-32 lap:pt-10 tab:px-20 ph:px-6 ph:pt-3">
                         <Header header={index ? index.header : ""} />
